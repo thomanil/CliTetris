e3caa849fb06055b82c94cdda7ed8c84d6fdc3a7
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2014-07-22 15:14:04 +0200

    Add test target

---
+++ CliTetrisTests/CliTetrisTests.m
@@ -0,0 +1,34 @@
+//
+//  CliTetrisTests.m
+//  CliTetrisTests
+//
+//  Created by Thomas Kjeldahl Nilsson on 22/07/14.
+//  Copyright (c) 2014 Thomas Kjeldahl Nilsson. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CliTetrisTests : XCTestCase
+
+@end
+
+@implementation CliTetrisTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
