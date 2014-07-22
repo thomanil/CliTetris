5e8d7ee3690c045a44b049ca5749d99e5f29d8a5
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2014-07-22 14:20:35 +0200

    First commit

--- CliTetris/main.m
+++ CliTetris/main.m
@@ -1,15 +1,6 @@
-//
-//  main.m
-//  CliTetris
-//
-//  Created by Thomas Kjeldahl Nilsson on 22/07/14.
-//  Copyright (c) 2014 Thomas Kjeldahl Nilsson. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[])
-{
+int main(int argc, const char * argv[]) {
 
     @autoreleasepool {
         
